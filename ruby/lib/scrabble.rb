91ca84694ade2dde798e8d0715b325291f4225d3
Steven Solomon <solomon.steven.m@gmail.com>
2018-12-13 21:40:29 -0500

    Add Ruby support

---
+++ ruby/lib/scrabble.rb
@@ -0,0 +1,2 @@
+module Scrabble
+end
---
+++ ruby/spec/scrabble_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require './lib/scrabble_data'
+
+describe 'Scrabble' do
+  it 'should do something' do
+    expect(true).to be(false)
+  end
+end
